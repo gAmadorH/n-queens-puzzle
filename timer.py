fdc2fb6b86b3082457ed4e420c256f19fbdcea24
Gonzalo Amador <gamador@cantilever.com.mx>
2019-10-21 12:16:31 -0500

    labels for seconds and minutes were added in timer wrapper
    timer wrapper was updated to show labels of seconds and minutes

--- timer.py
+++ timer.py
@@ -7,8 +7,15 @@ def timer(func):
     start_time = time.time()
     returned = func(*args, **kwargs)
     end_time = time.time()
-    duration_ms = (end_time - start_time) * 1000
+    duration = (end_time - start_time)
+    duration_ms = duration * 1000
+    duration_s = duration_ms/1000
+    duration_m = duration_s / 60
+
 
     print('"{0}" function has taken {1} ms'.format(func.__name__, duration_ms))
+    print('"{0}" function has taken {1} seconds'.format(func.__name__, duration_s))
+    print('"{0}" function has taken {1} minutes'.format(func.__name__, duration_m))
+
     return returned
   return wrapper
