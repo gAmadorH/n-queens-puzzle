025732b10c43ca5acf14215806bda5f133b0f31f
Gonzalo Amador <gamador@cantilever.com.mx>
2019-10-21 20:23:12 -0500

    database connection was added and solutions were saved in db

--- index.py
+++ index.py
@@ -1,9 +1,38 @@
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, ARRAY, select
+from sqlalchemy.sql import and_
 from NQueensSolver import NQueensSolver
 
-if __name__ == "__main__":
-  solver = NQueensSolver(8) 
 
-  for solve in solver.run():
-    print(solve)
-  
+if __name__ == "__main__":
+  engine = create_engine("postgresql://nQueensPuzzle:nQueensPuzzle@localhost:5432/nQueensPuzzle",echo = True)
+  metadata = MetaData()
+
+  Solutions = Table(
+    'solutions', metadata, 
+    Column('id', Integer, primary_key = True), 
+    Column('n', Integer), 
+    Column('board', ARRAY(Integer))
+  )
+
+  metadata.create_all(engine)
+
+  size = 5
+  solver = NQueensSolver(size)
+  solver.run()
+    
+  for solution in solver.get_solutions():
+    print(solution)
+    print(NQueensSolver.print_board(solution))
+
+    stmt = select([Solutions]).where(and_(
+      Solutions.c.n == size,
+      Solutions.c.board == solution
+    ))
+
+    selected_solutions = engine.execute(stmt).fetchone()
+
+    if selected_solutions is None:
+      insert_solutions = Solutions.insert().values(n = size, board = solution)
+      engine.execute(insert_solutions)
+
   print(solver.num_solutions)
