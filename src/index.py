2c635d25f50b2d7c4535efd1a69f2182119dfd19
Gonzalo Amador <gamador@cantilever.com.mx>
2019-10-24 12:22:49 -0500

    new structure was implemented

--- src/index.py
+++ src/index.py
@@ -3,7 +3,8 @@ from sqlalchemy.sql import and_
 from NQueensSolver import NQueensSolver
 
 if __name__ == "__main__":
-  engine = create_engine("postgresql://nQueensPuzzle:nQueensPuzzle@nqueenspuzzledb:5432/nQueensPuzzle",echo=False)
+  # engine = create_engine("postgresql://nQueensPuzzle:nQueensPuzzle@nqueenspuzzledb:5432/nQueensPuzzle",echo=False)
+  engine = create_engine("postgresql://nQueensPuzzle:nQueensPuzzle@localhost:5432/nQueensPuzzle",echo=False)
   metadata = MetaData()
 
   Solutions = Table(
@@ -15,7 +16,7 @@ if __name__ == "__main__":
 
   metadata.create_all(engine)
 
-  size = 7
+  size = 4
   solver = NQueensSolver(size)
   solver.run()
     
